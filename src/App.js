4bef3383108289566b9561825e861c3a4954aebd
ThomasA64 <thomas.arthur64@gmail.com>
2021-03-10 14:02:48 -0500

    add Cards Component

--- src/App.js
+++ src/App.js
@@ -3,18 +3,31 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 //Components:
 import Home from "./components/Home";
 import Menu from "./components/Menu";
+import Cards from "./components/Cards";
 
 //Styles:
 import "./styles/menu.css";
 import "./styles/styles.css";
 import "./styles/animations.css";
+import "./styles/cards.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Menu />
+
       <Switch>
-        <Route component={Home} path="/" exact />
+        <Route
+          path="/"
+          exact
+          render={() => (
+            <>
+              {" "}
+              <Home />
+              <Cards />{" "}
+            </>
+          )}
+        />
       </Switch>
     </BrowserRouter>
   );
---
+++ src/components/Cards.js
@@ -0,0 +1,94 @@
+import React from "react";
+
+export default function Cards() {
+  return (
+    <header className="showcase">
+      <div id="cards_landscape_wrap-2">
+        <div class="container">
+          <h1 className="bio">KNOWLEDGE</h1>
+          <div class="row">
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+              <a href="">
+                <div class="card-flyer">
+                  <div class="text-box">
+                    <div class="image-box">
+                      <img
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png"
+                        alt=""
+                      />
+                    </div>
+                    <div class="text-container">
+                      <h6>Love for Modern UI</h6>
+                      <p>
+                        Adept in HTML5/CSS3/JavascriptES6, as well as JS
+                        Frameworks such as React, and CSS frameworks such as
+                        Bootstrap. Assured in Responsive design.
+                        <br></br>
+                        <br></br>
+                        Experienced in implementing and maintaining CMS
+                        templates such as Wordpress and Wix.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </a>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+              <a href="">
+                <div class="card-flyer">
+                  <div class="text-box">
+                    <div class="image-box">
+                      <img
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png"
+                        alt=""
+                      />
+                    </div>
+                    <div class="text-container">
+                      <h6>FULL STACK</h6>
+                      <p>
+                        Confident in implementing middleWare such as ExpressJS
+                        for routing between client and server.
+                        <br></br>
+                        <br></br>
+                        Knowledgeable in the creation and implementation of
+                        databases using relational databases such as PostgreSQL.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </a>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+              <a href="">
+                <div class="card-flyer">
+                  <div class="text-box">
+                    <div class="image-box">
+                      <img
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png"
+                        alt=""
+                      />
+                    </div>
+
+                    <div class="text-container">
+                      <h6>Industry Standards</h6>
+                      <p>
+                        Knowledgeable in industry standards and best practices
+                        including Git Version control, Object-Oriented
+                        Programming and Test-Driven Development.
+                        <br></br>
+                        <br></br>
+                        Familiar with Ruby and Rails framework, Unit,
+                        Integration and End-to-End Testing with Jest, Cypress,
+                        Rspec and Capybara.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
