a51f8b1695b69f9c4973c23f81cd90375305a04a
ThomasA64 <thomas.arthur64@gmail.com>
2021-03-22 14:58:21 -0400

    added react-bootstrap Navbar component

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import Menu from "./components/Menu";
 import Cards from "./components/Cards";
+import NavBar from "./components/NavBar";
 
 //Styles:
 import "./styles/menu.css";
 import "./styles/styles.css";
 import "./styles/animations.css";
 import "./styles/cards.css";
+import "./styles/nav.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Menu />
-
+      <NavBar />
       <Switch>
         <Route
           path="/"
