e0594498dece042992c6cb4c600fcf85575372ba
ThomasA64 <thomas.arthur64@gmail.com>
2021-03-26 16:29:10 -0400

    switched TinyApp with Jungle Railsin Projects component, added link to delpoyment on Heroku

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -51,6 +51,28 @@ export default function Projects() {
         </div>
 
         <div classname="double-proj">
+          <a href="https://tiny-app-ta.herokuapp.com/login">
+            <div className="flip-card">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <img
+                    className="img-projects"
+                    src="./docs/TinyAppUser.png"
+                    alt="Project"
+                  />
+                </div>
+                <div className="flip-card-back">
+                  <h1>TinyApp</h1>
+                  <p>
+                    Tiny App is an app built through Node.js and Express.js to
+                    showcase Login features and Routing.
+                    <br></br>
+                    It allows the user to sign in and create custom short URLS.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </a>
           <div className="flip-card">
             <div className="flip-card-inner">
               <div className="flip-card-front">
@@ -68,29 +90,6 @@ export default function Projects() {
                   Web-Application using React.js, Express/Node.js and
                   PostrgesSQL. Styling is complete with Bootstrap and custom
                   CSS.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="flip-card">
-            <div className="flip-card-inner">
-              <div className="flip-card-front">
-                <img
-                  className="img-projects"
-                  src="./docs/JungleHome.png"
-                  alt="Project"
-                />
-              </div>
-              <div className="flip-card-back">
-                <h1>Jungle on Rails</h1>
-                <p>
-                  Jungle is a mini e-commerce application built with Ruby on
-                  Rails.
-                  <br></br>
-                  Choose from many wacky inventions including the Electric
-                  Chair, Human Feet Shoes and Modern Skateboards! Who needs
-                  plain boring common items found on Amazon when you have access
-                  to the world's most interesting items?!
                 </p>
               </div>
             </div>
