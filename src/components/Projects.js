41108734d977f61a23062fa4eb33df996315bd0d
ThomasA64 <thomas.arthur64@gmail.com>
2021-04-18 13:55:14 -0400

    added links for animail and scheduler to github on the projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -26,26 +26,28 @@ export default function Projects() {
               </div>
             </div>
           </a>
-          <div className="flip-card">
-            <div className="flip-card-inner">
-              <div className="flip-card-front">
-                <img
-                  className="img-projects"
-                  src="./docs/SchedulerMainPage.png"
-                  alt="Project"
-                />
-              </div>
-              <div className="flip-card-back">
-                <h1 className="h1desc">Interview Scheduler</h1>
-                <p className="description">
-                  React application that allows users to book and cancel
-                  interviews. Combine a concise API with a WebSocket server to
-                  build a realtime experience. Data is persisted by the API
-                  server using a PostgreSQL database.
-                </p>
+          <a href="https://github.com/ThomasA64/scheduler">
+            <div className="flip-card">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <img
+                    className="img-projects"
+                    src="./docs/SchedulerMainPage.png"
+                    alt="Project"
+                  />
+                </div>
+                <div className="flip-card-back">
+                  <h1 className="h1desc">Interview Scheduler</h1>
+                  <p className="description">
+                    React application that allows users to book and cancel
+                    interviews. Combine a concise API with a WebSocket server to
+                    build a realtime experience. Data is persisted by the API
+                    server using a PostgreSQL database.
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
+          </a>
         </div>
 
         <div classname="double-proj">
@@ -71,27 +73,29 @@ export default function Projects() {
               </div>
             </div>
           </a>
-          <div className="flip-card">
-            <div className="flip-card-inner">
-              <div className="flip-card-front">
-                <img
-                  className="img-projects"
-                  src="./docs/AnimailHome.png"
-                  alt="Project"
-                />
-              </div>
-              <div className="flip-card-back">
-                <h1 className="h1desc">Animail</h1>
-                <p className="description">
-                  Animail is a React application that allows users to send
-                  messages via their favourite animals. Full-Stack
-                  Web-Application using React.js, Express/Node.js and
-                  PostrgesSQL. Styling is complete with Bootstrap and custom
-                  CSS.
-                </p>
+          <a href="https://github.com/Samicap/AniMail">
+            <div className="flip-card">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <img
+                    className="img-projects"
+                    src="./docs/AnimailHome.png"
+                    alt="Project"
+                  />
+                </div>
+                <div className="flip-card-back">
+                  <h1 className="h1desc">Animail</h1>
+                  <p className="description">
+                    Animail is a React application that allows users to send
+                    messages via their favourite animals. Full-Stack
+                    Web-Application using React.js, Express/Node.js and
+                    PostrgesSQL. Styling is complete with Bootstrap and custom
+                    CSS.
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
+          </a>
         </div>
       </section>
     </>
