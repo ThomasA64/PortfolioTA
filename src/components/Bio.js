5b1ce2d8b846afee4a2699e7280fc36aec85814f
ThomasA64 <thomas.arthur64@gmail.com>
2021-04-20 21:49:10 -0400

    added styling to bio section as well as expanded the description

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -8,12 +8,20 @@ export default function Bio() {
       </div>
       <div>
         <p className="bio-description">
-          Hi, my name is Thomas and I am a recent graduate of Lighthouse Labs,
-          the leading development bootcamp in Canada and have received my
-          certification as a Full-Stack developer. This was an extremely
-          fulfilling experience as I was able to explore my passion for the web
-          industry and demonstrate my ability and willingness to learn in a
-          fast-paced, collaborative team environment.
+          Hi, my name is Thomas and I am a Full-Stack Developer. <br></br> I am
+          a recent graduate of Lighthouse Labs, the leading development bootcamp
+          in Canada, and have received my certification as a Full-Stack
+          developer. <br></br>
+          This was an extremely fulfilling experience as I was able to explore
+          my passion for the web industry and demonstrate my ability and
+          willingness to learn in a fast-paced, collaborative team environment.
+          <br></br>I have a huge passion for Technology and aspire to contribute
+          to the field through my skills and creativity.
+          <br></br>I have an abundance of knowledge of industry standards and
+          best practices, as well as a proven ability to learn new languages and
+          frameworks in a pressured environment. <br></br> I have particular
+          interest in interactive and user-friendly website and application
+          designs and in providing an outstanding customer experience.
         </p>
       </div>
     </section>
