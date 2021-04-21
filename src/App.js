fe76bd92b06945f66a8c7bae35c25b93b003821a
ThomasA64 <thomas.arthur64@gmail.com>
2021-04-20 21:25:28 -0400

    added description to bio page

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
           render={() => (
             <>
               {" "}
-              <Cards />{" "}
+              <Bio />{" "}
             </>
           )}
         />
--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,5 +1,19 @@
 import React from "react";
 
 export default function Bio() {
-  return <div></div>;
+  return (
+    <section className="bio-section">
+      <div></div>
+      <div>
+        <p>
+          I am a recent graduate of Lighthouse Labs, the leading development
+          bootcamp in Canada and have received my certification as a Full-Stack
+          developer. This was an extremely fulfilling experience as I was able
+          to explore my passion for the web industry and demonstrate my ability
+          and willingness to learn in a fast-paced, collaborative team
+          environment.
+        </p>
+      </div>
+    </section>
+  );
 }
