36aedeccd9a59d3ca5d83eba34f1f8553ac482e7
ThomasA64 <thomas.arthur64@gmail.com>
2021-05-05 20:12:15 -0400

    changed font

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -97,6 +97,53 @@ export default function Projects() {
             </div>
           </a>
         </div>
+        <div classname="double-proj">
+          <a href="https://tiny-app-ta.herokuapp.com/login">
+            <div className="flip-card">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <img
+                    className="img-projects"
+                    src="./docs/TinyAppUser.png"
+                    alt="Project"
+                  />
+                </div>
+                <div className="flip-card-back">
+                  <h1 className="h1desc">TinyApp</h1>
+                  <p className="description">
+                    Tiny App is an app built through Node.js and Express.js to
+                    showcase Login features and Routing.
+                    <br></br>
+                    It allows the user to sign in and create custom short URLS.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </a>
+          <a href="https://github.com/Samicap/AniMail">
+            <div className="flip-card">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <img
+                    className="img-projects"
+                    src="./docs/AnimailHome.png"
+                    alt="Project"
+                  />
+                </div>
+                <div className="flip-card-back">
+                  <h1 className="h1desc">Animail</h1>
+                  <p className="description">
+                    Animail is a React application that allows users to send
+                    messages via their favourite animals. Full-Stack
+                    Web-Application using React.js, Express/Node.js and
+                    PostrgesSQL. Styling is complete with Bootstrap and custom
+                    CSS.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
       </section>
     </>
   );
